f9f235c7bfce4ed9dd1fe182c400da766b37488d
ujg-mw-stage <67598919+ujg-mw-stage@users.noreply.github.com>
2020-06-30 14:06:16 -0400

    Delete sampleScript.m

--- sampleScript.m
+++
@@ -1,8 +0,0 @@
-%% Sample script: Creating an anonymous identity function
-% An identity function returns its input arguments. This script 
-% demonstrates one way to create an anonymous identity function.
-I = @(X) X;
-
-Y = I('?');
-
-disp(Y)
