d1d456c77ae78dad6d1b0a6b97464311901232a5
ujg-mw-new-fix <67598919+ujg-mw-new-fix@users.noreply.github.com>
2020-07-01 10:44:11 -0400

    Delete sampleFunction.m

--- sampleFunction.m
+++
@@ -1,8 +0,0 @@
-function [Y] = sampleFunction(X)
-%sampleFunction Identity function.
-%   Y = sampleFunction(X) Returns X.
-%
-%   Example: Get the same Unicode character passed as the argument.
-%      star = sampleFunction('?');
-Y=X;
-end
